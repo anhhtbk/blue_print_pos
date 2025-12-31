686945d141d79e3ad4bd020ca1360c2c22f5b693
anhhtbk <michealhoang8@gmail.com>
2025-12-31 13:35:01 +0700

    Update Logger.kt

--- android/src/main/kotlin/com/ayeee/blue_print_pos/Logger.kt
+++ android/src/main/kotlin/com/ayeee/blue_print_pos/Logger.kt
@@ -1,11 +1,6 @@
 package com.ayeee.blue_print_pos
 
-import android.util.Log
-
 object Logger {
     fun log(message: String) {
-        // if (BuildConfig.DEBUG) {
-        //     Log.d("BluePrintPos", message)
-        // }
     }
 }
